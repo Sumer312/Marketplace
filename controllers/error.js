33890b6c986dc685daa8fe3ff307769104ec67b0
Sumer Singh <84413141+Sumer312@users.noreply.github.com>
2023-02-11 00:48:44 +0530

    Delete error.js

--- controllers/error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const get404 = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-};
-exports.default = get404;
